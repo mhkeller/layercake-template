df084010bf28690b3424b0e5f192bbdd622afe0f
mhkeller <code@mhkeller.com>
2020-01-23 18:47:07 -0500

    remove conditional

--- src/components/AxisX.svelte
+++ src/components/AxisX.svelte
@@ -25,21 +25,19 @@
 	}
 </script>
 
-{#if $xScale}
-	<g class='axis x-axis'>
-		{#each tickVals as tick, i}
-			<g class='tick tick-{ tick }' transform='translate({$xScale(tick)},{$yScale.range()[0]})'>
-				{#if gridlines !== false}
-					<line y1='{$height * -1}' y2='0' x1='0' x2='0'></line>
-				{/if}
-				<text y='16' text-anchor='{textAnchor(i)}'>{formatTick(tick)}</text>
-			</g>
-		{/each}
-		{#if baseline === true}
-			<line class="baseline" y1='{$height + 0.5}' y2='{$height + 0.5}' x1='0' x2='{$width}'></line>
-		{/if}
-	</g>
-{/if}
+<g class='axis x-axis'>
+	{#each tickVals as tick, i}
+		<g class='tick tick-{ tick }' transform='translate({$xScale(tick)},{$yScale.range()[0]})'>
+			{#if gridlines !== false}
+				<line y1='{$height * -1}' y2='0' x1='0' x2='0'></line>
+			{/if}
+			<text y='16' text-anchor='{textAnchor(i)}'>{formatTick(tick)}</text>
+		</g>
+	{/each}
+	{#if baseline === true}
+		<line class="baseline" y1='{$height + 0.5}' y2='{$height + 0.5}' x1='0' x2='{$width}'></line>
+	{/if}
+</g>
 
 <style>
 	.tick {
