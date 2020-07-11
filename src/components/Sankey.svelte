ff0fc3663850d0e50f2e672f3260455a4ecef9d2
mhkeller <code@mhkeller.com>
2020-07-11 15:31:17 -0400

    add sankey

---
+++ src/components/Sankey.svelte
@@ -0,0 +1,64 @@
+<script>
+	import { getContext } from 'svelte';
+	import * as Sankey from 'd3-sankey';
+
+	const { data, width, height } = getContext('LayerCake');
+
+	/* --------------------------------------------
+	 * Allow for dynamic coloring
+	 */
+	export let colorLinks = d => 'rgba(0, 0, 0, .2)';
+	export let colorNodes = d => '#333';
+	export let colorText = d => '#263238';
+
+	export let nodeWidth = 5;
+	export let nodePadding = 10;
+
+	$: sankey = Sankey.sankey()
+		.nodeAlign(Sankey.sankeyLeft)
+		.nodeWidth(nodeWidth)
+		.nodePadding(nodePadding)
+		.nodeId(d => d.id)
+		.size([$width, $height])
+		.linkSort(null);
+
+	$: sankeyData = sankey($data);
+
+	$: link = Sankey.sankeyLinkHorizontal();
+
+	$: fontSize = $width <= 320 ? '8px' : '12px';
+</script>
+
+<g class="sankey-layer">
+	<g class='link-group'>
+		{#each sankeyData.links as d}
+			<path
+				d={link(d)}
+				fill={'none'}
+				stroke={colorLinks(d)}
+				stroke-opacity='0.5'
+				stroke-width={d.width} />
+		{/each}
+	</g>
+	<g class='rect-group'>
+		{#each sankeyData.nodes as d, i}
+			<rect
+				x={d.x0}
+				y={d.y0}
+				height={d.y1 - d.y0}
+				width={d.x1 - d.x0}
+				fill={colorNodes(d)} />
+			<text
+				x={d.x0 < $width / 4 ? d.x1 + 6 : d.x0 - 6}
+				y={(d.y1 + d.y0) / 2}
+				style={`fill: ${colorText(d)};
+								alignment-baseline: 'middle';
+								font-size: ${fontSize};
+								text-anchor: ${d.x0 < $width / 4 ? 'start' : 'end'};
+								pointer-events: 'none';
+								user-select: 'none';`}>
+				{d.id}
+			</text>
+		{/each}
+	</g>
+</g>
