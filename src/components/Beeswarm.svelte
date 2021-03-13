5970a2d546817f4f11d1b55015bb401da7d9f325
mhkeller <code@mhkeller.com>
2021-03-12 20:15:52 -0500

    add beeswarm

---
+++ src/components/Beeswarm.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { getContext } from 'svelte';
+	import { forceSimulation, forceX, forceY, forceCollide } from 'd3-force';
+
+	const { data, xGet, width, height, zGet } = getContext('LayerCake');
+
+	const nodes = $data.map((d) => ({ ...d }));
+
+	export let r = 4;
+	export let spacing = 0.5;
+	export let xStrength = 0.95;
+	export let yStrength = 0.075;
+	export let strokeColor = '#fff';
+
+	let radius = r;
+	$: {
+		radius = $width < 400 ? r / 2 : r;
+	}
+
+	$: simulation = forceSimulation(nodes)
+		.force('x', forceX().x(d => $xGet(d)).strength(xStrength))
+		.force('y', forceY().y($height / 2).strength(yStrength))
+		.force('collide', forceCollide(radius + spacing))
+		.stop();
+
+	$: {
+		for ( var i = 0,
+			n = Math.ceil(Math.log(simulation.alphaMin()) / Math.log(1 - simulation.alphaDecay()));
+			i < n;
+			++i ) {
+			simulation.tick();
+		}
+	}
+</script>
+
+<g class='bee-group'>
+	{#each simulation.nodes() as node}
+		<circle
+			fill='{$zGet(node)}'
+			stroke='{strokeColor}'
+			stroke-width='{spacing / 2}'
+			cx='{node.x}'
+			cy='{node.y}'
+			r='{radius}'
+			/>
+	{/each}
+</g>
