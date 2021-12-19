43ff77aa8c715c1bd436e75c511f36c4af8184d3
mhkeller <code@mhkeller.com>
2021-12-18 22:32:05 -0500

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script>
+	import { LayerCake, Svg } from 'layercake';
+
+	import Line from './components/Line.svelte';
+	import Area from './components/Area.svelte';
+	import AxisX from './components/AxisX.svelte';
+	import AxisY from './components/AxisY.svelte';
+
+	import points from './data/points.csv';
+
+	const xKey = 'myX';
+	const yKey = 'myY';
+
+	points.forEach(row => {
+		row[yKey] = +row[yKey];
+	});
+</script>
+
+<style>
+	.chart-container {
+		width: 100%;
+		height: 100%;
+	}
+</style>
+
+<div class="chart-container">
+	<LayerCake
+		padding={{ right: 10, bottom: 20, left: 25 }}
+		x={xKey}
+		y={yKey}
+		yDomain={[0, null]}
+		data={points}
+	>
+		<Svg>
+			<AxisX/>
+			<AxisY/>
+			<Line/>
+			<Area/>
+		</Svg>
+	</LayerCake>
+</div>
