cea2799a34e4ea806774391767b7f8f41effde76
Robert Gieseke <rob.g@web.de>
2023-07-20 18:47:12 +0200

    Add component from layercake repo

--- src/routes/_components/AxisX.svelte
+++ src/routes/_components/AxisX.svelte
@@ -1,40 +1,44 @@
+<!--
+	@component
+	Generates an SVG x-axis. This component is also configured to detect if your x-scale is an ordinal scale. If so, it will place the markers in the middle of the bandwidth.
+ -->
 <script>
-	/**
-		Generates an SVG x-axis. This component is also configured to detect if your x-scale is an ordinal scale. If so, it will place the markers in the middle of the bandwidth.
-	*/
-
 	import { getContext } from 'svelte';
-
 	const { width, height, xScale, yRange } = getContext('LayerCake');
 
 	/** @type {Boolean} [gridlines=true] - Extend lines from the ticks into the chart space */
-	let gridlines = true;
+	export let gridlines = true;
+
 	/** @type {Boolean} [tickMarks=false] - Show a vertical mark for each tick. */
 	export let tickMarks = false;
+
 	/** @type {Boolean} [baseline=false] – Show a solid line at the bottom. */
 	export let baseline = false;
+
 	/** @type {Boolean} [snapTicks=false] - Instead of centering the text on the first and the last items, align them to the edges of the chart. */
 	export let snapTicks = false;
+
 	/** @type {Function} [formatTick=d => d] - A function that passes the current tick value and expects a nicely formatted value in return. */
-	export let formatTick = (/** @type {any} */ d) => d;
-	/** @type {Number|Array<Number|String>|Function|Undefined} [ticks] - If this is a number, it passes that along to the [d3Scale.ticks](https://github.com/d3/d3-scale) function. If this is an array, hardcodes the ticks to those values. If it's a function, passes along the default tick values and expects an array of tick values in return. If nothing, it uses the default ticks supplied by the D3 function. */
+	export let formatTick = d => d;
+
+	/** @type {Number|Array|Function} [ticks] - If this is a number, it passes that along to the [d3Scale.ticks](https://github.com/d3/d3-scale) function. If this is an array, hardcodes the ticks to those values. If it's a function, passes along the default tick values and expects an array of tick values in return. If nothing, it uses the default ticks supplied by the D3 function. */
 	export let ticks = undefined;
-	/**	@type {Number} [xTick=0] - TODO */
+
+	/** @type {Number} [xTick=0] - How far over to position the text marker. */
 	export let xTick = 0;
+
 	/** @type {Number} [yTick=16] - The distance from the baseline to place each tick value. */
 	export let yTick = 16;
 
 	$: isBandwidth = typeof $xScale.bandwidth === 'function';
 
-	$: tickVals = Array.isArray(ticks)
-		? ticks
-		: isBandwidth
-		? $xScale.domain()
-		: typeof ticks === 'function'
-		? ticks($xScale.ticks())
-		: $xScale.ticks(ticks);
+	$: tickVals = Array.isArray(ticks) ? ticks :
+		isBandwidth ?
+			$xScale.domain() :
+			typeof ticks === 'function' ?
+				ticks($xScale.ticks()) :
+					$xScale.ticks(ticks);
 
-	/** @param {number} i */
 	function textAnchor(i) {
 		if (snapTicks === true) {
 			if (i === 0) {
@@ -49,8 +53,8 @@
 </script>
 
 <g class="axis x-axis" class:snapTicks>
-	{#each tickVals as tick, i}
-		<g class="tick tick-{i}" transform="translate({$xScale(tick)},{$yRange[0]})">
+	{#each tickVals as tick, i (tick)}
+		<g class="tick tick-{i}" transform="translate({$xScale(tick)},{Math.max(...$yRange)})">
 			{#if gridlines !== false}
 				<line class="gridline" y1={$height * -1} y2="0" x1="0" x2="0" />
 			{/if}
@@ -59,12 +63,12 @@
 					class="tick-mark"
 					y1={0}
 					y2={6}
-					x1={xTick || isBandwidth ? $xScale.bandwidth() / 2 : 0}
-					x2={xTick || isBandwidth ? $xScale.bandwidth() / 2 : 0}
+					x1={isBandwidth ? $xScale.bandwidth() / 2 : 0}
+					x2={isBandwidth ? $xScale.bandwidth() / 2 : 0}
 				/>
 			{/if}
 			<text
-				x={xTick || isBandwidth ? $xScale.bandwidth() / 2 : 0}
+				x={isBandwidth ? ($xScale.bandwidth() / 2 + xTick) : xTick}
 				y={yTick}
 				dx=""
 				dy=""
